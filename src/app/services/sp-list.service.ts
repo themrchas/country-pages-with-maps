179d23e9a3d8df5a33b1fe85f75f07e431814295
Lisa.Blumenberg <lisa.blumenberg@socom.mil>
2018-12-03 09:41:39 -0500

    Added SP List utility function

--- src/app/services/sp-list.service.ts
+++ src/app/services/sp-list.service.ts
@@ -9,6 +9,24 @@ export class SpListService {
 
   constructor(private httpClient: HttpClient) { }
 
+  getListItems(listWeb: string, listName: string, order?: string, filter?: string, rowLimit?: number) {
+    let reqUrl = `${listWeb}/_api/web/lists/getByTitle('${listName}')/items`;
+    reqUrl += order || filter || rowLimit ? '?' : '';
+    if (order) {
+      reqUrl += '$orderby=' + order;
+    }
+    if (filter) {
+      reqUrl += order ? '&' : '';
+      reqUrl += '$filter=' + filter;
+    }
+    if (top) {
+      reqUrl += order || filter ? '&' : '';
+      reqUrl += '$top=' + rowLimit;
+    }
+
+    return this.httpClient.get(reqUrl, ConfigProvider.spGetHttpOptions());
+  }
+
   getView(listWeb: string, listName: string, viewGuid: string) {
     return this.httpClient.get(`${listWeb}/_api/web/lists/getByTitle('${listName}')/views(guid'${viewGuid}')`,
       ConfigProvider.spGetHttpOptions());
