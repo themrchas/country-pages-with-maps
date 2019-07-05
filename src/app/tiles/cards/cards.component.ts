dd62ae64eae08c6bb927a770e13ec4f6498b756d
Lisa.Blumenberg <lisa.blumenberg@socom.mil>
2019-07-05 10:57:19 -0600

    Troubleshooting NGX with IE.  Fixed highlighting in map tile.  Switched to using FileLeafRef instead of expanded File/Name when retrieving name of file in doc library

--- src/app/tiles/cards/cards.component.ts
+++ src/app/tiles/cards/cards.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, AfterViewChecked } from '@angular/core';
 import { TileComponent } from '../tile/tile.component';
 import { Country } from 'src/app/model/country';
 import { DataLayerService } from 'src/app/services/data-layer.service';
 import { DataSource } from 'src/app/model/dataSource';
 import { SpRestService } from 'src/app/services/sp-rest.service';
 import { BaseDataService } from 'src/app/services/baseDataService';
+import {NumberCardComponent} from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.scss']
 })
-export class CardsComponent implements OnInit, TileComponent {
+export class CardsComponent implements OnInit, TileComponent, AfterViewChecked {
   @Input() settings: any;
   @Input() country: Country;
+  @ViewChild(NumberCardComponent) numCard: NumberCardComponent;
 
+  svg: any;
+  isIE: boolean;
   view: any[] = undefined;
   colorScheme = {
     domain: ['#5AA454', '#A10A28', '#C7B42C']
@@ -24,19 +28,19 @@ export class CardsComponent implements OnInit, TileComponent {
   /* single = [
     {
       'name': 'Germany',
-      'value': 8940000
+      'value': '8940000\nTest'
     },
     {
-      'name': 'USA',
+      'name': '<strong>USA</strong><p>Here is some info</p>',
       'value': 5000000
     },
     {
       'name': 'Kenya',
       'value': 7200000
     }
-  ];
+  ]; */
 
-  multi = [
+  /* multi = [
     {
       'name': 'Germany',
       'series': [
@@ -83,6 +87,30 @@ export class CardsComponent implements OnInit, TileComponent {
 
   ngOnInit() {
     this.loadCards(this.country);
+
+    const ua = navigator.userAgent;
+    const M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    this.isIE = (/trident/i.test(M[1])) ? true : false;
+  }
+
+  ngAfterViewChecked() {
+    // Very hacky way to get labels to display in IE11 (foreignObject not supported)
+    if (this.isIE && this.cardElement && !this.svg) {
+      this.svg = this.cardElement.nativeElement.childNodes[0].childNodes[0].childNodes[0].childNodes[0];
+      /*$('foreignObject').each((index, el) => {
+        const label = $(el).text();
+        $(el).replaceWith(
+          '<text class="value-text" x="23.5" y="50.5" style="fill: rgb(18, 33, 17); font-size: 18pt;">' +
+          label +
+          '</text>');
+      });
+      $('ngx-charts-chart').html($('ngx-charts-chart').html());*/
+      /*$('.value-text').each((index, el) => {
+        $(el).html('<tspan dy="1.2em">Hi</tspan><tspan dy="1.2em">there<tspan>');
+      });*/
+      // $('.value-text').text('hiya');
+      // $('.num-card ngx-charts-chart').html($('ngx-charts-chart').html());
+    }
   }
 
   loadCards(country: Country) {
